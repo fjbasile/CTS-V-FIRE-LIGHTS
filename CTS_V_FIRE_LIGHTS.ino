c165f3341c3b80a069f034ab3a4164dc6996882b
fjbasile <fjbasile@icloud.com>
2020-05-21 20:40:46 -0500

    Create CTS_V_FIRE_LIGHTS.ino

---
+++ CTS_V_FIRE_LIGHTS.ino
@@ -0,0 +1,30 @@
+#include <Adafruit_CircuitPlayground.h>
+
+int red;
+int blue;
+int green;
+
+const int DELAY = 10;
+const int lowDel = 5;
+const int highDel = 400;
+
+const int RANDLOW = 30;
+const int RANDHI = 256;
+
+void setup()
+{
+  CircuitPlayground.begin(); 
+  randomSeed(analogRead(0));
+}
+ 
+void loop()
+{ 
+    red = random(RANDLOW, RANDHI);
+    blue = random(RANDLOW, RANDHI);
+    green = random(RANDLOW, RANDHI);
+    //for (int light = 0; light < 10; light ++)
+    //{
+      CircuitPlayground.setPixelColor(0, red, blue, green);
+    //}
+    delay(random(lowDel,highDel)); 
+}
